cdaee04a7be05212fce74ef5cc7d9848ac215aa9
RinuthaC <89367531+RinuthaC@users.noreply.github.com>
2024-02-20 12:37:12 +0530

    Add files via upload

---
+++ apply.js
@@ -0,0 +1,18 @@
+const person = {
+    fullName: function(city, country) {
+      return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+  }
+  
+  const person1 = {
+    firstName:"John",
+    lastName: "Doe"
+  }
+  
+  person.fullName.call(person1, "Oslo", "Norway");
+  const b=10;
+
+  const a=apply()
+  function apply() {
+    console.log(b);
+  }
